b992ebd907527da39acb3ce068cf128a641814c4
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-05 15:46:36 +0300

    finish the part 1 step 2

--- main.py
+++ main.py
@@ -3,3 +3,7 @@
 numbers = [3, 7, 10, 15]
 res = map(lambda num: num + 10, numbers)
 print(list(res))
+# step 2
+prices = [100, 50, 200, 80]
+res = map(lambda num: num * 1.17, prices)
+print(list(res))
