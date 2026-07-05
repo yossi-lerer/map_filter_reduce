1401a1f91099c7bef07ee3c4d4555219fba69a64
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-05 15:48:50 +0300

    finish the part 1 step 3

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ print(list(res))
 prices = [100, 50, 200, 80]
 res = map(lambda num: num * 1.17, prices)
 print(list(res))
+# step 3
+words = ["cat", "elephant", "dog", "python"]
+res = map(lambda word: len(word), words)
+print(list(res))
