857807d1a871912f421338d7c69bf72d5de21ccf
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-05 16:44:13 +0300

    finish the part 2 step 10

--- main.py
+++ main.py
@@ -103,3 +103,12 @@ tasks = [
 ]
 res = filter(lambda task: True if task["done"] == False and task["priority"] <= 3 else False, tasks)
 print(list(res))
+# step 10
+students = [
+    {"name": "Noa", "grade": 90, "attendance": 95},
+    {"name": "Dan", "grade": 55, "attendance": 100},
+    {"name": "Rina", "grade": 80, "attendance": 70},
+    {"name": "Eli", "grade": 75, "attendance": 85}
+]
+res = filter(lambda student: True if student["grade"] >= 70 and student["attendance"] >= 80 else False, students)
+print(list(res))
